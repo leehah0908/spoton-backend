5df2f4504f8f5d724d65957f19c0aca779ef7060
leehah <leehah0908@naver.com>
2024-11-14 23:55:31 +0900

    코드 정리

--- src/main/java/com/spoton/spotonbackend/common/auth/CustomLogoutSuccessHandler.java
+++ src/main/java/com/spoton/spotonbackend/common/auth/CustomLogoutSuccessHandler.java
@@ -1,6 +1,5 @@
 package com.spoton.spotonbackend.common.auth;
 
-import com.spoton.spotonbackend.common.dto.CommonResDto;
 import com.spoton.spotonbackend.user.entity.LoginType;
 import com.spoton.spotonbackend.user.entity.User;
 import com.spoton.spotonbackend.user.repository.UserRepository;
@@ -13,13 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
@@ -30,23 +25,15 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
     private final RedisTemplate<String, Object> redisTemplate;
-    private final RestTemplate restTemplate;
 
     @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
     private String kakaoAppKey;
-
-    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String naverAppKey;
-
-    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String googleAppKey;
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request,
                                 HttpServletResponse response,
                                 Authentication authentication) throws IOException, ServletException {
 
-        System.out.println("필터 로그아웃으로 옴!");
         String redirectUri = "http://localhost:3000";
         LoginType loginType = null;
 
@@ -62,7 +49,6 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
                         loginType = user.getLoginType();
                         redisTemplate.delete(user.getEmail());
-                        System.out.println(loginType);
 
                         cookie.setValue("");
                         cookie.setPath("/");
@@ -71,7 +57,6 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
                         cookie.setHttpOnly(true);
                         cookie.setAttribute("SameSite", "None");
                         response.addCookie(cookie);
-                        System.out.println("쿠키 담기 성공!");
 
                     } catch (Exception e) {
                         response.setStatus(HttpStatus.UNAUTHORIZED.value());
@@ -106,7 +91,9 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
                 response.getWriter().write(googleLogoutUrl);
             }
         } else {
-            response.sendRedirect(redirectUri);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(redirectUri);
         }
     }
 }
--- src/main/java/com/spoton/spotonbackend/common/configs/WebConfig.java
+++ src/main/java/com/spoton/spotonbackend/common/configs/WebConfig.java
@@ -1,10 +1,6 @@
 package com.spoton.spotonbackend.common.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,11 +16,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(86400);
-    }
-
-    // restTemplate 빈 등록
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
